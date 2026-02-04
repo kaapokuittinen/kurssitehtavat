7bc7eade886ed5b582d6d9b3323fe2a678428dc2
kaapokuittinen <kaapo.kuittinen@gmail.com>
2026-02-04 13:39:16 +0200

    tehtävä 2.3 valmis

---
+++ Viikko 1/2.3.py
@@ -0,0 +1,7 @@
+import math
+kanta = float(input("anna kanta: "))
+korkeus = float(input("anna korkeus: "))
+pinta_ala = kanta*korkeus
+piiri = 2*kanta + 2*korkeus
+print("pinta-ala: " + str(pinta_ala))
+print("piiri: " + str(piiri))

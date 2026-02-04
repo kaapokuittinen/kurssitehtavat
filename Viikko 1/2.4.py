49ac2359c3127d5fe603034cddfc5fbda25b2a4d
kaapokuittinen <kaapo.kuittinen@gmail.com>
2026-02-04 13:44:17 +0200

    tehtävä 2.4 valmis

---
+++ Viikko 1/2.4.py
@@ -0,0 +1,10 @@
+import math
+ensimmainen_luku = float(input("anna ensimmäinen luku: "))
+toinen_luku = float(input("anna toinen luku: "))
+kolmas_luku = float(input("anna kolmas luku: "))
+summa = ensimmainen_luku + toinen_luku + kolmas_luku
+tulo = ensimmainen_luku * toinen_luku * kolmas_luku
+keskiarvo = (ensimmainen_luku + toinen_luku + kolmas_luku)/2
+print("summa: " + str(summa))
+print("tulo: " + str(tulo))
+print("keskiarvo: " + str(keskiarvo))
